582c2df278181ce39c38ab9113abf41e27335ea0
yu-teacher <yusm1231@gmail.com>
2023-09-18 16:10:55 +0900

    최초커밋

---
+++ src/main/java/com/pro/miniGame/MiniGameApplication.java
@@ -0,0 +1,13 @@
+package com.pro.miniGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniGameApplication.class, args);
+	}
+
+}
